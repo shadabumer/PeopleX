6f6fd4e15ade426ec9c7b9330324780f849df4b6
Shadab Umer <shadabum3r@gmail.com>
2022-06-11 07:57:48 +0530

    code clean up

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -19,22 +19,6 @@ const routes: Routes = [
                 component: AddProjectComponent,
                 canActivate: [AuthGuard]
             },
-            { path: 'charts', loadChildren: () => import('./charts/charts.module').then((m) => m.ChartsModule) },
-            { path: 'tables', loadChildren: () => import('./tables/tables.module').then((m) => m.TablesModule) },
-            { path: 'forms', loadChildren: () => import('./form/form.module').then((m) => m.FormModule) },
-            {
-                path: 'bs-element',
-                loadChildren: () => import('./bs-element/bs-element.module').then((m) => m.BsElementModule)
-            },
-            { path: 'grid', loadChildren: () => import('./grid/grid.module').then((m) => m.GridModule) },
-            {
-                path: 'components',
-                loadChildren: () => import('./bs-component/bs-component.module').then((m) => m.BsComponentModule)
-            },
-            {
-                path: 'blank-page',
-                loadChildren: () => import('./blank-page/blank-page.module').then((m) => m.BlankPageModule)
-            }
         ]
     }
 ];
